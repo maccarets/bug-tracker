5fe35ccda76f03ebc2e32736d22938a56c462114
maccarets <maccarets@gmail.com>
2023-07-24 13:05:42 +0300

    edit login and registration

--- bug_tracking_system/settings.py
+++ bug_tracking_system/settings.py
@@ -82,8 +82,13 @@ AUTH_USER_MODEL = 'bug_tracker.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "bug_tracker",
+        'USER': "postgres",
+        "PASSWORD": "letubu17",
+        "HOST": "localhost",
+        "PORT": "5432"
+
     }
 }
 
