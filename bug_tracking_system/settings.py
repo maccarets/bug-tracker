8eecd30e8a8fa2adbb959f2d5445810bc198d1b9
maccarets <maccarets@gmail.com>
2023-07-25 20:20:03 +0300

    secret key remove

--- bug_tracking_system/settings.py
+++ bug_tracking_system/settings.py
@@ -28,8 +28,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-yz@)5qqc4o)fbeyzdab2euvs16z&c=-*j^93*1$v=8)2hv=aov")
-
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
